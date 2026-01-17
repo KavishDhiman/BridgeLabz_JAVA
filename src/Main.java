7c6977f09b6f7adc72b5a8ceae26b8a4657bd8d3
KavishDhiman <149812635+KavishDhiman@users.noreply.github.com>
2026-01-17 10:58:36 +0530

    Refactor Main class and update main method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-void main() {
-    
-    System.out.Println("Hello and Welcome");
-
-   
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+    }
 }
